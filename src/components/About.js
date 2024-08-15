e73385634d0791743d2751c889d1f3ff1852f316
Syamala Chandrasekhar <scxyp@umsystem.edu>
2024-08-14 22:15:15 -0500

    checking the changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className="about" id="about">
             <h2>About Me</h2>
             <p>
-                I'm a Computer Science graduate from the University of Missouri with a strong background in full-stack development.
+                I'm a Computer Science graduate from the University of Missouri with a strong background in full-stack development and exposure to the latest technologies. I have experience in developing web applications using React, Node.js, Express, and MongoDB. I have also worked on projects using Java, Python, and C++.
                 I have professional experience in software engineering in both the U.S. and India, and I am currently seeking full-time roles in software engineering.
             </p>
         </section>
